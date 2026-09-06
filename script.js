8e442926e4a721023ee3cfe43428277e2774c9c5
moisekalemba05-pdg <moisekalemba05@gmail.com>
2026-09-06 21:16:35 +0530

    optimisation and clarification of the code

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 const wordsList = ["Cachalot", "Petunia", "Serviette"];
 const phrasesList = ["my Parents", "my Sisters", "my Brother"];
 let score = 0
-let userInput = ""
-console.log("choose between the 'wordsList' and the 'phrasesList'. " );
-let userAnswer = prompt("Enter 'word' for 'wordsList' or 'phrase' for 'phrasesList'.");
-
-while( userAnswer !== "word" && userAnswer !== "phrase" ){
-userAnswer = prompt("Enter 'word' for 'wordsList' or 'phrase' for 'phrasesList'.");
+let userChoice = prompt("chose between the 'word' and the 'phrase'.");
+while( userChoice !== "word" && userChoice !== "phrase" ){
+    userChoice = prompt("chose between the 'word' and the 'phrase'.");
+}
+if(userChoice === "word"){
+    for(let i = 0; i < wordsList.length; i++){
+        let userWord = prompt("Enter the word number " + (i + 1) + " of the Words' list.");
+        if( userWord === wordsList[i]){
+            score++ ;
+        }   
+    }
+}
+else{
+    for (let i = 0; i < phrasesList.length; i++){
+        let userPhrase = prompt("Enter the phrase number " +  (i + 1) + " of the Phrases' list.");
+        if( userPhrase === phrasesList[i]){
+            score++ ;
+        }
+    }
 }
 
-if(userAnswer === "word"){
-for(let i = 0; i < wordsList.length; i++){
-userInput = prompt("Enter the word number " + (i + 1) + " of the Words' list.");
-if( userInput === wordsList[i]){
-score++ ;
-}}}
-else{
-for (let i = 0; i < phrasesList.length; i++){
-userInput =  prompt("Enter the phrase number " +  (i + 1) + " of the Phrases' list.");
-if( userInput === phrasesList[i]){
-score++ ;
-}}}
-
-console.log("Your final score is " + score + " out of " + (userAnswer === "word" ? wordsList.length : phrasesList.length));
+console.log("Your final score is " + score + " out of " + (userChoice === "word" ? wordsList.length : phrasesList.length));
